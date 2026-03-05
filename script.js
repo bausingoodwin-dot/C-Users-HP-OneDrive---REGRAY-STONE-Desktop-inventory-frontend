df5ed376cf7adfec25c8e4910aade417d720dde4
bausingoodwin-dot <bausingoodwin@gmail.com>
2026-03-05 17:29:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -216,11 +216,3 @@ function renderChart(){
 
 // --- INITIAL RENDER ---
 renderInventory();
-document.getElementById("logoutBtn").onclick = function(){
-
-localStorage.removeItem("loggedIn");
-
-window.location.href = "login.html";
-
-}
-
