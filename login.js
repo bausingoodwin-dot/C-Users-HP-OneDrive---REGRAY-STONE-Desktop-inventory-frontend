428f319d805dffb8ec4fc7cd9eefa42f1e35b0e1
bausingoodwin-dot <bausingoodwin@gmail.com>
2026-03-07 11:43:54 +0800

    Update login.js

--- login.js
+++ login.js
@@ -2,8 +2,8 @@ document.getElementById("loginForm").addEventListener("submit",function(e){
 
 e.preventDefault();
 
-const username = document.getElementById("username").value;
-const password = document.getElementById("password").value;
+const username = document.getElementById("username").value.trim();
+const password = document.getElementById("password").value.trim();
 
 if(username === "admin" && password === "12345"){
 
